1fbd04ae5826ae3b81a86766bb79841361b4f166
Cuantico <liarevalo@me.com>
2020-02-10 12:57:38 -0600

    Updates

---
+++ precache-manifest.de98e1bf15c5cd349da5a1cb5dae3f30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9377035cd1b1f47166762c7c1850043b",
+    "url": "/Reading-List-Client/index.html"
+  },
+  {
+    "revision": "14a6ebca8b86a4934e93",
+    "url": "/Reading-List-Client/static/css/main.256c35f6.chunk.css"
+  },
+  {
+    "revision": "5cdff2d1ed48b9ec0c93",
+    "url": "/Reading-List-Client/static/js/2.d0d67ccd.chunk.js"
+  },
+  {
+    "revision": "14a6ebca8b86a4934e93",
+    "url": "/Reading-List-Client/static/js/main.56bd7b23.chunk.js"
+  },
+  {
+    "revision": "e5c5867861f35ce3a88d",
+    "url": "/Reading-List-Client/static/js/runtime~main.8725be1d.js"
+  }
+]);
